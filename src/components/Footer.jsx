1d730e7ec5378e0842461efa79dbfc8c3030c431
Jeevith K <104653693+JeevithK@users.noreply.github.com>
2025-04-02 00:37:11 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 export function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        Created by <span className="creator-name">Jeevith K</span>
-      </div>
-    </footer>
+    <></>
   );
 }
